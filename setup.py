010d3e9269bee0dfc154f03aa8660c9366250f09
MinchinWeb <w_minchin@hotmail.com>
2022-03-19 10:38:22 -0600

    [requirements] update to invoke 1.7 to support Python 3.10

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ INSTALL_REQUIRES = [
     # also vendorized minchin.text >= 6.0.0
     "colorama >= 0.2.5",
     "gitpython >= 3.0.6",
-    "invoke >= 0.20",
+    "invoke >= 1.7.0",
     "isort >= 5",
     "semantic_version",
     "twine >= 1.11.0",
@@ -104,6 +104,7 @@ CLASSIFIERS = [
     "Programming Language :: Python :: 3.7",
     "Programming Language :: Python :: 3.8",
     "Programming Language :: Python :: 3.9",
+    "Programming Language :: Python :: 3.10",
     "Programming Language :: Python :: 3 :: Only",
     "Natural Language :: English",
     "Intended Audience :: Developers",
